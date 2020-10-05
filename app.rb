7acdb3aefb048ae878ffac99b0b34fe74a463591
Ralph Mallett <rmallett12@gmail.com>
2020-10-05 14:01:08 +0100

    feat: create named cat route

--- app.rb
+++ app.rb
@@ -10,7 +10,13 @@ get '/secret' do
   'You found the secret page...'
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = %w[Amigo Oscar Viking].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
