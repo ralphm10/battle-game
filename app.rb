73aa080b8e3af3ff19d26792c026b9b271f1a7d7
Ralph Mallett <rmallett12@gmail.com>
2020-10-05 15:02:15 +0100

    chore: discard practice files, update project directory

--- app.rb
+++ app.rb
@@ -1,26 +1,10 @@
-require 'sinatra'
+require 'sinatra/base'
 
-set :session_secret, 'super secret'
+class Battle < Sinatra::Base
 
 get '/' do
-  'Hello!'
+  'Hello Battle!'
 end
 
-get '/secret' do
-  'You found the secret page...'
-end
-
-get '/random-cat' do
-  @name = %w[Amigo Oscar Viking].sample
-  erb(:index)
-end
-
-get '/cat-form' do
-  erb :cat_form
-end
-
-post '/named-cat' do
-  p params
-  @name = params[:name]
-  erb(:index)
+run! if app_file == $0
 end
