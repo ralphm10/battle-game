92d52e5e86e0317f8c92035a32e304ecd06a5fb0
Ralph Mallett <rmallett12@gmail.com>
2020-10-06 11:52:52 +0100

    refactor: add sign in and play web helper method

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'name entry' do
   scenario 'player enters name which is then displayed' do
-    visit '/'
-    fill_in('player_1_name', with: 'John')
-    fill_in('player_2_name', with: 'Paul')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content 'John vs. Paul'
   end
 end
--- spec/features/view_hitpoints_spec.rb
+++ spec/features/view_hitpoints_spec.rb
@@ -1,9 +1,6 @@
 feature 'viewing hitpoints' do
   scenario 'view player 2 hit points' do
-    visit '/'
-    fill_in('player_1_name', with: 'John')
-    fill_in('player_2_name', with: 'Paul')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content 'Paul: 60 Points'
   end
 end
