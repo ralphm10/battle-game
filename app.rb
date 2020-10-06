54552ca055fba5f95f0758a8f94ae7522d325933
Ralph Mallett <rmallett12@gmail.com>
2020-10-06 16:38:22 +0100

    refactor: game class takes 2 players on instantiation

--- app.rb
+++ app.rb
@@ -14,21 +14,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
+    player1 = Player.new(params[:player_1_name])
+    player2 = Player.new(params[:player_2_name])
+    $game = Game.new(player1, player2)
     redirect '/play'
   end
 
   get '/play' do
-    @player1 = $player_1
-    @player2 = $player_2
+    @game = $game
     erb(:play)
   end
 
   get '/attack' do
-    @player1 = $player_1
-    @player2 = $player_2
-    Game.new.attack(@player2)
+    @game = $game
+    @game.attack(@game.player2)
     erb(:attack)
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,8 +2,21 @@ require 'game'
 require 'player'
 
 describe Game do
+  subject(:game) { described_class.new(george, ringo) }
   let(:george) { double :player }
   let(:ringo) { double :player }
+
+  describe '#player1' do
+    it 'accepts first player' do
+      expect(game.player1).to eq george
+    end
+  end
+
+  describe '#player2' do
+    it 'accepts second player' do
+      expect(game.player2).to eq ringo
+    end
+  end
 
   describe '#attack' do
     it 'attacks a player' do
