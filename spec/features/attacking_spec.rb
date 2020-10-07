fb1c6e612e46223a1df9dc4b51eb8e5fd69865ca
Ralph Mallett <rmallett12@gmail.com>
2020-10-07 15:45:32 +0100

    Feat: both opponents can attack

--- spec/features/attacking_spec.rb
+++ spec/features/attacking_spec.rb
@@ -10,4 +10,11 @@ feature 'attacking' do
     click_link 'OK'
     expect(page).to have_content('Paul: 50 Points')
   end
+  scenario 'attack player 1 and receive confirmation' do
+    sign_in_and_play
+    click_button('attack opponent')
+    click_link 'OK'
+    click_button('attack opponent')
+    expect(page).to have_content('Paul attacked John!')
+  end
 end
